5a2fc53bf34827604c92a7ea84b1664477111327
Steve <steve3300@naver.com>
2022-04-11 06:33:59 +0900

    first commit

---
+++ 3/index.js
@@ -0,0 +1,12 @@
+let check = document.getElementById('check');
+let output = document.getElementById('p');
+
+check.addEventListener('click', function(){
+    a = 3.141;
+    let input = document.getElementById('input').value;
+    if(input == a){
+        output.innerHTML = 'You are correct';
+    }else{
+        output.innerHTML = 'You are incorrect, please try again';
+    }
+});
